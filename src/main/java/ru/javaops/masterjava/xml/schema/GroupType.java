86064811dae81570c0296c9edddaa98fb810f77b
Dmitriy Kurochkin <kurochkin.dm@gmail.com>
2022-01-14 00:09:51 +0300

    fix

--- src/main/java/ru/javaops/masterjava/xml/schema/GroupType.java
+++ src/main/java/ru/javaops/masterjava/xml/schema/GroupType.java
@@ -1,8 +1,9 @@
 
 package ru.javaops.masterjava.xml.schema;
 
+import jakarta.xml.bind.annotation.XmlType;
+
 import javax.xml.bind.annotation.XmlEnum;
-import javax.xml.bind.annotation.XmlType;
 
 
 /**
